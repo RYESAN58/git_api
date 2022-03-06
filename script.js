87eb9974d7786d8b7ba2f45ff4ed096cb99a8ed7
RYESAN58 <92766524+RYESAN58@users.noreply.github.com>
2022-03-06 02:38:50 -0500

    updated so that you can type in any usernane from github
    yay

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 async function getapi(){
-    var response = await fetch("https://pokeapi.co/api/v2/pokemon/1")
-    var _data = await response.json
-    console.log(_data)
+    const x = prompt("Which user will you 'd like to see")
+    var response = await fetch(`https://api.github.com/users/${x}`)
+    var coderData = await response.json();
+    console.log(coderData)
+    document.getElementById("box").innerHTML = `<h2>${coderData.name} has ${coderData.followers} Followers </h2>`;
+    document.getElementById("pic").src= coderData.avatar_url;
+
 }
-getapi()
+
+// var data = {
+//     "orders": [
+//     {
+//         "orderno": 784692,
+//         "date": "June 30, 2088 1:54:23 AM",
+//         "trackingno": "TN000391",
+//         "customer": {
+//             "custid": 11045,
+//             "fname": "Sue",
+//             "lname": "Hatfield",
+//             "address": "1409 Silver Street",
+//             "city": "Ashland",
+//             "state": "NE",
+//             "zip": 68003
+//         }
+//     },
+//         {
+//         "orderno": 784693,
+//         "date": "March 3, 2088 8:18:14 PM",
+//         "trackingno": "TN000468",
+//             "customer": {
+//             "custid": 11045,
+//             "fname": "Sue",
+//             "lname": "Hatfield",
+//             "address": "1409 Silver Street",
+//             "city": "Ashland",
+//             "state": "NE",
+//             "zip": 68003
+//             }
+//         }
+//     ]
+// }
+
+// console.log(typeof(data));
+// console.log(Array.isArray(data));
+// console.log(Array.isArray(data.orders));
+// console.log(data.orders[0].orderno);
+// console.log(data);
+
+
+// console.log(data);
+// console.log(data.orders[0]);
+// console.log(data.orders[0].customer.address);
+
 
